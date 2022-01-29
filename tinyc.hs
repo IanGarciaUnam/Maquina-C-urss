6b0ef863d4238cb8fd309335ab5470c2b4d965fd
Esjag <marioagd@ciencias.unam.mx>
2022-01-28 21:45:30 -0600

    Se implementaron las transiciones para las llamadas a función.

--- tinyc.hs
+++ tinyc.hs
@@ -3,6 +3,20 @@ module TinyC where
  
  type Identificador = String
 
+ {-
+   En el caso de las llamadas a función hay un problema.
+
+   Necesitamos el precerbar de alguna forma las expresiones que queremos asociar con los parametros
+   de la función, esto en la nota se hace mediante el marco que guarda en la pila el momento en el
+   que se hace una llamada a función. Sin embargo, en esta implementación no contiene dicha informa-
+   ción. Por lo que se buscaron diferentes soluciones que pudieran resolve el problema. Entonces se
+   proponen 3 posibles:
+
+   1. Se define una versión alterativa en donde el marco guarda la información de las expresiones.
+   2. Se define una función auxiliar para poder evluar las expresiones hasta llevarlas a un valor.
+   3. Se presupone que en la llamada, ya todas las expresiones son valores.
+ -}
+
  -- Expresiones
  data Expr = Num Int
             | Bo Bool
@@ -38,6 +52,20 @@ module TinyC where
  data Env = MtEnv | As Var Env | Star Env Env deriving(Show,Eq)
 
 -- Marcos de la pila
+
+ {-
+   Definición alternativa para los marcos de llamada a función, en esta versión se maneja una lista de
+   expresiones en conjunto con el identificador de la función.
+
+ data Marco = VardecM Identificador
+            | AsigM Identificador
+            | SecuM Stmt
+            | IfM Stmt Stmt
+            | IfOM Stmt
+            | ReturnM 
+            | FuncallM Identificador [Epr]
+            deriving(Show,Eq)
+ -}
  data Marco = VardecM Identificador
             | AsigM Identificador
             | SecuM Stmt
@@ -138,6 +166,49 @@ module TinyC where
  -- While
  trans (E p l g (Right (While exp stm))) = E p l g (Right (IfO exp (Secu stm (While exp stm))))
 
+ -- Llamadas a función
+ {-
+   Versión con la definición alternativa del marco para llamadas a función.
+
+ trans (E p l g (Left (Funcall id []))) = case look g id of
+    Just (F lid stm) -> E p (Star g l) l (Right stm)
+    _ -> error "La función llamada, no ha sido definida."
+ trans (E p l g (Left (Funcall id (e:lexp)))) = case look g id of
+    Just (F lid stm) -> E (Top (FuncallM id lexp) p) (As (id, F lid stm) l) g (Left e)
+    _ -> error "La función llamada, no ha sido definida."
+ trans (R (Top (FuncallM id1 []) p) (As (id2, F (i:lid) stm) l) g (Left v)) = case v of
+    Num n -> E p (Star g l) (As (i, N n) l) (Right stm)
+    Bo b -> E p (Star g l) (As (i, B b) l) (Right stm)
+    Fun li st -> E p (Star g l) (As (i, F li st) l) (Right stm)
+    _ -> error "Un paramétro de la llamada, no es un valor."
+ trans (R (Top (FuncallM id1 (e:lexp)) p) (As (id2, F (i:lid) stm) l) g (Left v)) = case v of
+    Num n -> E (Top (FuncallM id1 lexp) p) (As (id2, F lid stm) (As (i, N n) l)) g (Left e)
+    Bo b -> E (Top (FuncallM id1 lexp) p) (As (id2, F lid stm) (As (i, B b) l)) g (Left e)
+    Fun li stm -> E (Top (FuncallM id1 lexp) p) (As (id2, F lid stm) (As (i, F li stm) l)) g (Left e)
+    _ -> error "Un paramétro de la llamada, no es un valor."
+ -}
+
+ {-
+   Versíon usando la alternativa de una función auxiliar.
+
+ trans s@(E p g l (Left (Funcall id exprs))) = case look g id of
+    Just (F ids stm) -> transFun s ids MtEnv stm
+    _ -> error "La función llamada, no ha sido definida."
+ -}
+
+ {-
+   Función usando el hecho de que todas las expresiones listadas son un valor.
+ -}
+ trans (E p l g (Left (Funcall id exprs))) = case look g id of
+    Just (F ids stm) -> R (Top (FuncallM id) p) l (As (id, F ids stm) g) (Left (Funcall id exprs))
+    _ -> error "La función llamada, no ha sido definida."
+ trans (R (Top (FuncallM id1) p) l (As (id2, F ids stm) g) (Left (Funcall id3 []))) = E p (Star g l) l (Right  stm)
+ trans (R (Top (FuncallM id1) p) l (As (id2, F (i:ids) stm) g) (Left (Funcall id3 (e : exprs)))) = case e of
+    Num n -> R (Top (FuncallM id1) p) (As (i, N n) l) (As (id2, F ids stm) g) (Left (Funcall id3 exprs))
+    Bo b -> R (Top (FuncallM id1) p) (As (i, B b) l) (As (id2, F ids stm) g) (Left (Funcall id3 exprs))
+    Fun li st -> R (Top (FuncallM id1) p) (As (i, F li st) l) (As (id2, F ids stm) g) (Left (Funcall id3 exprs))
+    _ -> error "Uno de los paramétros de la llamada no es un valor."
+
  -- Return
  trans (E p g l (Right (Return exp))) = E (Top ReturnM p) g l (Left exp)
  trans (R (Top ReturnM p) (Star g l1) l2 e@(Left (Num n))) = R p l1 g e
@@ -146,3 +217,17 @@ module TinyC where
 
  -- Momentaneo
  trans _ = error "Estado inválido"
+
+ {-
+   Función auxiliar para resolver las transiciones en funciones.
+ -}
+ transFun :: State -> [Identificador] -> Env -> Stmt -> State
+ transFun (E p l g (Left (Funcall id []))) i env stm = E p (Star g l) env (Right stm)
+ transFun (E p l g (Left (Funcall id (e : le)))) (i : ids) env stm = case trans (E p l g (Left e)) of
+    (E p g l (Left val)) -> case val of
+       Num n -> transFun (E p l g (Left (Funcall id le))) ids (As (i, N n) env) stm
+       Bo b -> transFun (E p l g (Left (Funcall id le))) ids (As (i, B b) env) stm
+       Fun lids stm2 -> transFun (E p l g (Left (Funcall id le))) ids (As (i, F lids stm2) env) stm
+       _ -> error "Uno de los parametros no puede ser reducido a un vamor"
+    _ -> error "Fallo en la evaluación"
+ transFun _ _ _ _ = error ""
